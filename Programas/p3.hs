33060066f386aea8094e85e403b6d4cb2c05a60f
nicolaciar <nicolas.laciar@mi.unc.edu.ar>
2025-10-15 18:24:26 -0300

    practico 3 hs

--- Programas/p3.hs
+++ Programas/p3.hs
@@ -1,3 +1,16 @@
+--- Ejercicio 1
+f :: Int -> Int
+f x = 5*x
+
+duplica :: Int -> Int
+duplica a = a+a
+
+multiplicar :: Int -> Int -> Int
+multiplicar x y = x*y
+
+por2 :: Int -> Int
+por2 y = 2*y
+
 --- Ejercicio 2
 prom2 :: Double -> Double -> Double
 prom2 x y = (x+y) / 2.0
@@ -21,7 +34,25 @@ esMultiplo2 :: Int -> Bool
 esMultiplo2 n = (n `mod` 2 == 0)
 
 -- Ejercicio 8
-
 esMultiploDe :: Int -> Int -> Bool
 esMultiploDe x y = (y `mod` x == 0)
 
+-- Ejercicio 9
+esBisiesto :: Int -> Bool
+esBisiesto x = (x `mod` 400 == 0) || (x `mod` 4 ==0 && x `mod` 100 /= 0)
+
+-- Ejercicio 10
+
+max3 :: Double -> Double -> Double ->Double
+max3 x y z = z `max` (x `max` y)
+
+min3 :: Double -> Double -> Double -> Double
+min3 x y z = z `min` (x `min` y)
+
+dispersion :: Double -> Double -> Double -> Double
+dispersion x y z = max3 x y z - min3 x y z
+
+
+-- Ejercicio 14
+segundo3 :: (Int, Int, Int) -> Int
+segundo3 (a,b,c) = b
